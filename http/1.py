0c1a457bfb3d32313eb62b9bc0b0b874e406ade8
Виктория Баранова <114149496+viktoriia234@users.noreply.github.com>
2023-03-11 11:53:30 +0300

    Update 1.py

--- http/1.py
+++ http/1.py
@@ -1,7 +1,17 @@
 import requests
 
 
-p = requests.get("https://cataas.com/cat/says/hello%20world!")
+p = requests.get("https://cataas.com/cat")
+out = open("img.jpg", "wb")
+out.write(p.content)
+out.close()
+
+p = requests.get("https://cataas.com/cat?type=or")
+out = open("img.jpg", "wb")
+out.write(p.content)
+out.close()
+
+p = requests.get("https://cataas.com/cat?filter=pixel")
 out = open("img.jpg", "wb")
 out.write(p.content)
 out.close()
